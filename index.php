6605029dcd402585e401989067ddcc28ce612bc0
NEHA MUSHTAQ <neha75010@yahoo.com>
2022-11-13 13:10:10 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'Car.php';
+require_once 'Bicycle.php';
+require_once 'Truck.php';
+
+$bicycle = new Bicycle('blue',1);
+$rockrider = new Bicycle('yellow',1);
+$tornado = new Bicycle('black',1);
+
+$audi = new Car('white', 8, 'essence');
+$mercedes = new Car('grey', 4, 'gazoil');
+
+$tanker = new Truck('white', 3, 'fuel', 34000);
